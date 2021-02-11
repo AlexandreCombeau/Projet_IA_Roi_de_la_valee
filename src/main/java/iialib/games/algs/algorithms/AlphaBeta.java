5fbb0d7edd6d9e8dce23797f366ce442bfae1439
LiliZr <liliaizri99@gmail.com>
2021-02-11 13:53:18 +0100

    MAJ AlphaBeta

--- src/main/java/iialib/games/algs/algorithms/AlphaBeta.java
+++ src/main/java/iialib/games/algs/algorithms/AlphaBeta.java
@@ -1,5 +1,7 @@
 package iialib.games.algs.algorithms;
 
+import java.util.ArrayList;
+
 import iialib.games.algs.GameAlgorithm;
 import iialib.games.algs.IHeuristic;
 import iialib.games.model.IBoard;
@@ -10,7 +12,7 @@ public class AlphaBeta<Move extends IMove,Role extends IRole,Board extends IBoar
 
 	// Constants
 	/** Defaut value for depth limit */
-	private final static int DEPTH_MAX_DEFAUT = 4;
+	private final static int DEPTH_MAX_DEFAUT = 3;
 
 		// Attributes
 		/** Role of the max player 
@@ -54,12 +56,58 @@ public class AlphaBeta<Move extends IMove,Role extends IRole,Board extends IBoar
 		
 	@Override
 	public Move bestMove(Board board, Role playerRole) {
-		// TODO Auto-generated method stub
-		return null;
+		Move bestMove = maxMin(board, playerRole, 0,  IHeuristic.MIN_VALUE, IHeuristic.MAX_VALUE).moveReturn;
+		System.out.println("nbNodes = " + nbNodes + "\t nbLeaves = " + nbLeaves);
+		return bestMove;
+	}
+
+	
+	private AlphaBetaReturn maxMin(Board board, Role role, int depth, int alpha, int beta) {
+		if( depth> depthMax ||board.isGameOver()) {
+			nbLeaves++;
+			return new AlphaBetaReturn(h.eval(board, role),null);
+		}else {
+			ArrayList<Move> possibleMoves = board.possibleMoves(role);
+			Move bestMove = possibleMoves.get(0);
+			
+			for(Move m : possibleMoves) {
+				if(depth < depthMax) nbNodes++;
+				
+				int value = minMax(board.play(m,role), playerMinRole, depth+1, alpha, beta).heuristicReturn;
+				if(alpha<value) {
+					alpha = value;
+					bestMove = m;
+				}
+				if(alpha>=beta) {
+					return new AlphaBetaReturn(beta, bestMove);
+				}
+			}
+			return new AlphaBetaReturn(alpha, bestMove);
+		}
 	}
 	
-	private AlphaBetaReturn alphabeta(Board board,Role role, int depth,int alpha, int beta) {
-		return null;
+	private AlphaBetaReturn minMax(Board board, Role role, int depth, int alpha, int beta) {
+		if( depth> depthMax || board.isGameOver()) {
+			nbLeaves++;
+			return new AlphaBetaReturn(h.eval(board, role),null);
+		}else {
+			ArrayList<Move> possibleMoves = board.possibleMoves(role);
+			Move bestMove = possibleMoves.get(0);
+			
+			for(Move m : possibleMoves) {
+				if(depth < depthMax) nbNodes++;
+				
+				int value = maxMin(board.play(m, role), playerMaxRole, depth+1, alpha, beta).heuristicReturn;
+				if(beta>value) {
+					beta = value;
+					bestMove = m;
+				}
+				if(alpha>=beta) {
+					return new AlphaBetaReturn(alpha, bestMove);
+				}
+			}
+			return new AlphaBetaReturn(beta, bestMove);
+		}
 	}
 	
 	private class AlphaBetaReturn{
